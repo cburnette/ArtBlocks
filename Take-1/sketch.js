191b123a516b0aa336ce31c0a09d5e3ab3db7d1c
Chad Burnette <chadburnette@me.com>
2021-08-15 23:14:42 -0400

    wiggle

--- Take-1/sketch.js
+++ Take-1/sketch.js
@@ -7,54 +7,57 @@ function setup() {
 function draw() {
     background(200)
 
-    noFill()
-    strokeWeight(5)
-    beginShape()
-    vertex(760*M, 120*M)
-    quadraticVertex(700*M, 190*M, 600*M, 230*M)
-    quadraticVertex(621*M, 236*M, 620*M, 240*M)
-    quadraticVertex(500*M, 390*M, 380*M, 500*M)
-    quadraticVertex(325*M, 370*M, 255*M, 370*M)
-    quadraticVertex(150*M, 390*M, 130*M, 520*M)
-    quadraticVertex(0*M, 560*M, 10*M, 620*M)
-    quadraticVertex(20*M, 750*M, 120*M, 885*M)
-    quadraticVertex(290*M, 1000*M, 390*M, 880*M)
-    quadraticVertex(350*M, 790*M, 375*M, 755*M)
-    quadraticVertex(400*M, 720*M, 515*M, 710*M)
-    quadraticVertex(490*M, 625*M, 420*M, 600*M)
-    quadraticVertex(550*M, 440*M, 680*M, 320*M)
-    quadraticVertex(685*M, 320*M, 690*M, 340*M)
-    quadraticVertex(780*M, 250*M, 875*M, 230*M)
-    quadraticVertex(790*M, 150*M, 760*M, 120*M)
-    endShape()
+    for (let i=0; i<20; i++) {
+        noFill()
+        strokeWeight(R.randNum(.1,.5))
+        beginShape()
+        vertex(860*M, 20*M)
+        wiggle(800*M, 90*M, 700*M, 130*M)
+        wiggle(721*M, 146*M, 720*M, 140*M)
+        wiggle(500*M, 390*M, 380*M, 500*M)
+        wiggle(325*M, 370*M, 255*M, 370*M)
+        wiggle(150*M, 390*M, 130*M, 520*M)
+        wiggle(0*M, 560*M, 10*M, 620*M)
+        wiggle(20*M, 750*M, 120*M, 885*M)
+        wiggle(290*M, 1000*M, 390*M, 880*M)
+        wiggle(350*M, 790*M, 375*M, 755*M)
+        wiggle(400*M, 720*M, 515*M, 710*M)
+        wiggle(490*M, 625*M, 420*M, 600*M)
+        wiggle(550*M, 440*M, 680*M, 320*M)
+        wiggle(785*M, 220*M, 790*M, 250*M)
+        wiggle(880*M, 150*M, 975*M, 130*M)
+        wiggle(890*M, 50*M, 860*M, 20*M)
+        endShape()
+    }
+    
 }
 
-function windowResized() {
-    tempHeight = window.innerHeight
-    tempWidth = tempHeight*ratio
-    const dim = Math.min(tempWidth, tempHeight)
-    M = dim / defaultSize
-    console.log(`M: ${M}`)
-    resizeCanvas(tempWidth, tempHeight);
+function wiggle(cx, cy, px, py) {
+    let cRange = 20
+    let pRange = 5
+    cx += R.randNum(-cRange,cRange)
+    cy += R.randNum(-cRange,cRange)
+    px += R.randNum(-pRange,pRange)
+    py += R.randNum(-pRange,pRange)
+    return quadraticVertex(cx,cy,px,py)
 }
 
 // Foundation stuff from here down
-
 class Random {
     constructor(seed) {
         this.seed = seed
     }
-    random_dec() {
+    randDec() {
         this.seed ^= this.seed << 13
         this.seed ^= this.seed >> 17
         this.seed ^= this.seed << 5
         return ((this.seed < 0 ? ~this.seed + 1 : this.seed) % 100000000) / 100000000
     }
-    random_num(a, b) {
-        return a+(b-a)*this.random_dec()
+    randNum(a, b) {
+        return a+(b-a)*this.randDec()
     }
-    random_int(a, b) {
-        return Math.floor(this.random_num(a, b+1))
+    randInt(a, b) {
+        return Math.floor(this.randNum(a, b+1))
     }
 }
 
@@ -70,6 +73,15 @@ tokenData = {
     //hash: "0x11ac16678959949c12d5410212301960fc496813cbc3495bf77aeed738579738", 
     hash: random_hash(),
     tokenId: "123000456"
+}
+
+function windowResized() {
+    tempHeight = window.innerHeight
+    tempWidth = tempHeight*ratio
+    const dim = Math.min(tempWidth, tempHeight)
+    M = dim / defaultSize
+    console.log(`M: ${M}`)
+    resizeCanvas(tempWidth, tempHeight);
 }
 
 const defaultSize = 1000
