136ed1b3ea44038b1148131260e9952898fe3d21
Chad Burnette <chadburnette@me.com>
2021-08-20 16:18:49 -0400

    no message

--- take-2/sketch.js
+++ take-2/sketch.js
@@ -9,6 +9,31 @@ function draw() {
     colorMode(HSL)
     background(31,100,92)
 
+    // ellipseMode(CENTER);
+    // let steps = 6;
+    // for (let i = 0; i <= steps; i++) {
+    //     let t = i / steps;
+    //     let x = curvePoint(5*M, 5*M, 73*M, 73*M, t);
+    //     let y = curvePoint(26*M, 26*M, 24*M, 61*M, t);
+    //     ellipse(x, y, 5*M, 5*M);
+    //     x = curvePoint(5*M, 73*M, 73*M, 15*M, t);
+    //     y = curvePoint(26*M, 24*M, 61*M, 65*M, t);
+    //     ellipse(x, y, 5*M, 5*M);
+    // }
+
+    curve(200*M,100*M,800*M,100*M,850*M,600*M,850*M,600*M)
+    curve(850*M,600*M,850*M,600*M,850*M,900*M,450*M,900*M)
+
+    beginShape()
+    curveVertex(700,100)
+    curveVertex(700,100)
+    curveVertex(730,400)
+    curveVertex(600,500)
+    curveVertex(700,700)
+    curveVertex(700,700)
+    endShape()
+
+    fill(31,10,92, 0.2)
     for (let i=0; i<R.randNum(1,3); i++) {
         strokeWeight(R.randNum(.2,.6))
         stroke(31,R.randNum(0,100),4)
@@ -32,16 +57,30 @@ function draw() {
         endShape()
     }  
 
-    downstroke(200*M,200*M,180*M,750*M)
+    downstroke(200*M,200*M,180*M,760*M, 100, 34)
+    downstroke(280*M,190*M,250*M,780*M, 100, 34)
+
+    ellipseMode(CENTER)
+    noFill()
+    let steps = 6
+    for (let i = 0; i <= steps; i++) {
+        let t = i / steps
+        let x = curvePoint(700, 730, 600, 700, t)
+        let y = curvePoint(100, 400, 500, 700, t)
+        ellipse(x, y, 5, 5)
+        downstroke(x,y,x,y+300, 15, 200)
+    }
 }
 
-function downstroke(x1,y1,x2,y2) {
+// Foundation stuff from here down
+
+function downstroke(x1,y1,x2,y2, maxWidth, hue) {
     noFill();
 
-    for (let i=0; i<R.randNum(1300,1900); i++) {
+    for (let i=0; i<R.randNum(13*maxWidth,19*maxWidth); i++) {
         strokeWeight(R.randNum(0.2,1.2));
-        stroke(34,100,R.randNum(45,55),R.randNum(0.1,0.8))
-        x1p = x1 + R.randNum(0,100)
+        stroke(hue,100,R.randNum(45,55),R.randNum(0.1,0.8))
+        x1p = x1 + R.randNum(0,R.randNum(maxWidth-(maxWidth*.8), maxWidth))
         x2p = x2 + (R.randNum(0.7,1.05)*(x1p-x1)) //x2 + R.randNum(0,100)
         y1p = y1 + R.randNum(-10,20)
         y2p = y2 + R.randNum(-10,20)
@@ -59,22 +98,26 @@ function curveVertexWiggle(px, py) {
     return curveVertex(px,py)
 }
 
-// Foundation stuff from here down
 class Random {
     constructor(seed) {
         this.seed = seed
+        this.originalSeed = seed
     }
     randDec() {
         this.seed ^= this.seed << 13
         this.seed ^= this.seed >> 17
         this.seed ^= this.seed << 5
-        return ((this.seed < 0 ? ~this.seed + 1 : this.seed) % 100000000) / 100000000
+        return ((this.seed < 0 ? ~this.seed + 1 : this.seed) % 1000) / 1000
     }
     randNum(a, b) {
         return a+(b-a)*this.randDec()
     }
     randInt(a, b) {
         return Math.floor(this.randNum(a, b+1))
+    }
+
+    reset() {
+        this.seed = this.originalSeed
     }
 }
 
@@ -98,6 +141,7 @@ function windowResized() {
     const dim = Math.min(tempWidth, tempHeight)
     M = dim / defaultSize
     console.log(`M: ${M}`)
+    R.reset()
     resizeCanvas(tempWidth, tempHeight);
 }
 
